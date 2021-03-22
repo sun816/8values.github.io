f6df79db5d38171d7d3238084d5c54e6068b94fb
sun816 <sunjoypie@gmail.com>
2021-03-22 16:46:06 +0800

    up

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "当你获得3060显卡你会很原意带给SJP玩儿.",
+        "question": "当你获得3060显卡你会很愿意带给SJP玩儿.",
         "effect": {
             "econ": 10,
             "dipl": 0,
