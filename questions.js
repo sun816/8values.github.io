c02184fc63afcaa1c9041828c869ba6cecfdc2e0
sun816 <sunjoypie@gmail.com>
2021-03-24 14:19:39 +0800

    up

--- questions.js
+++ questions.js
@@ -4,16 +4,16 @@ questions = [
         "effect": {
             "econ": 10,
             "dipl": 0,
-            "govt": -5,
+            "govt": 0,
             "scty": 0
         }
     },
 	    {
         "question": "2?.",
         "effect": {
-            "econ": -10,
+            "econ": 10,
             "dipl": 0,
-            "govt": -5,
+            "govt": -10,
             "scty": 0
         }
     },
@@ -21,44 +21,8 @@ questions = [
         "question": "3.",
         "effect": {
             "econ": 10,
-            "dipl": 15,
-            "govt": -5,
-            "scty": 0
-        }
-    },
-	    {
-        "question": "4.",
-        "effect": {
-            "econ": -20,
-            "dipl": 5,
-            "govt":15,
-            "scty": 0
-        }
-    },
-	    {
-        "question": "当5.",
-        "effect": {
-            "econ": 20,
-            "dipl": -10,
-            "govt": 12,
-            "scty": 0
-        }
-    },
-	    {
-        "question": "当你获得3060显卡你会很愿意带给SJP玩儿.",
-        "effect": {
-            "econ": 10,
             "dipl": 0,
-            "govt": -5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "当同事请求你代为刷卡的时候你退怯了",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 0,
+            "govt": 10,
             "scty": 0
         }
     }
