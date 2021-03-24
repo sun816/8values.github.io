189d85d6bbd4ccbe279446d05c39f70d7e1ae18d
sun816 <sunjoypie@gmail.com>
2021-03-24 14:37:12 +0800

    up

--- questions.js
+++ questions.js
@@ -11,18 +11,18 @@ questions = [
 	    {
         "question": "2?.",
         "effect": {
-            "econ": 10,
+            "econ": 0,
             "dipl": 0,
-            "govt": -10,
+            "govt": 0,
             "scty": 0
         }
     },
 	    {
         "question": "3.",
         "effect": {
-            "econ": 10,
+            "econ": 0,
             "dipl": 0,
-            "govt": 10,
+            "govt": 0,
             "scty": 0
         }
     }
